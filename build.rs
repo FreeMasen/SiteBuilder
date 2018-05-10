e2639693284a9dcce556ebad39d7285a3ee69e8a
robert masen <r@robertmasen.pizza>
2018-05-10 13:42:14 -0500

    update build.rs and deploy keys

--- build.rs
+++ build.rs
@@ -1,12 +1,15 @@
 #[cfg(windows)]
 extern crate windres;
-
 #[cfg(windows)]
 use windres::Build;
-
+#[cfg(windows)]
+use std::{env::var_os, path::PathBuf};
 #[cfg(windows)]
 fn main() {
-    Build::new().compile("site-builder.rc").unwrap();
+    if Some(arg) = var_os("APPVEYOR_BUILD_FOLDER") {
+        let path = PathBuf::from(arg);
+        Build::new().compile(path.join("site-builder.rc")).unwrap();
+    }
 }
 
 #[cfg(not(windows))]
