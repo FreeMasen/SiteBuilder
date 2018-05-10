7e7eb1a1d46167a8394f6451a4400e6ba88b3bac
robert masen <r@robertmasen.pizza>
2018-05-10 14:59:45 -0500

    !

--- build.rs
+++ build.rs
@@ -9,7 +9,7 @@ fn main() {
     if let Ok(path) = env::current_dir() {
         let rc_path = path.join("site-builder.rc");
         if !rc_path.exists() {
-            panic("site-builder.rs is not found at path {:?}", rc_path);
+            panic!("site-builder.rs is not found at path {:?}", rc_path);
         }
         Build::new().compile(rc_path).unwrap();
     }
