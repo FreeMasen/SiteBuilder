c25300736b279b3258fe4108757f8400b1b0bc00
robert masen <r@robertmasen.pizza>
2018-05-10 14:31:02 -0500

    moar deploy stuff

--- build.rs
+++ build.rs
@@ -3,11 +3,10 @@ extern crate windres;
 #[cfg(windows)]
 use windres::Build;
 #[cfg(windows)]
-use std::{env::var_os, path::PathBuf};
+use std::{env};
 #[cfg(windows)]
 fn main() {
-    if let Some(arg) = var_os("APPVEYOR_BUILD_FOLDER") {
-        let path = PathBuf::from(arg);
+    if let Ok(path) = env::current_dir() {
         Build::new().compile(path.join("site-builder.rc")).unwrap();
     }
 }
